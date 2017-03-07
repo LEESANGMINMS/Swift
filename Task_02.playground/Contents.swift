95cbbf813f063838a8f7eaabb209e7bfca3840b6
sang minlee <sangminlee@sangui-MacBook-Pro.local>
2017-03-08 00:43:32 +0900

    task02

---
+++ Task_02.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class calculator {
+    var number1:Int = 5
+    var number2:Int = 8
+    
+    func describe() {
+        print(number1+number2)
+        print(number2-number1)
+        print(number1*number2)
+        print(number2/number1)
+    }
+}
+
+var task02 = calculator()
+task02.describe()
+
