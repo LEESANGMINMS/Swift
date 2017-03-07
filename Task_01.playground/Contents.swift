86aea7f128982474c27df88441b67bcd38fc3191
sang minlee <sangminlee@sangui-MacBook-Pro.local>
2017-03-08 00:42:49 +0900

    task01

---
+++ Task_01.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//var str = "Hello, playground"
+
+var grades:Int = 79
+
+if grades >= 90 {
+    print("A")
+}
+if grades >= 80 {
+    print("B")
+}
+if grades >= 70 {
+    print("C")
+} else {
+    print("F")
+}
+
+
+
