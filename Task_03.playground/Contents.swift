ff66a8ec76688d6cd762fad6ad8aeb5e7616f62c
sang minlee <sangminlee@sangui-MacBook-Pro.local>
2017-03-09 01:38:11 +0900

    task_03

---
+++ Task_03.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//var str = "Hello, playground"
+
+
+let dia = ["oo1oo", "o111o", "11111", "o111o", "oo1oo"]
+for diamond in dia {
+    print(diamond)
+}
